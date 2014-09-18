593f980644af195c670e210efa99955a97796f92
Chris Mosetick <chris@revolutionanalytics.com>
2014-09-18 02:19:52 +0000

    added doit.R script

---
+++ gen-pkg-data/doit.R
@@ -0,0 +1,11 @@
+#!/usr/local/bin/Rscript
+
+setwd("/home/mran-user")
+
+source("/home/mran-user/gen-pkg-data/0-shared-scripts.R")
+source("/home/mran-user/gen-pkg-data/0-config.R")
+source("/home/mran-user/gen-pkg-data/1-all-package-data.R")
+source("/home/mran-user/gen-pkg-data/2-all-taskview-data.R")
+
+# graphs - not ready yet!
+# source("/Users/sheriair2/MRANrcode/gen-pkg-data/make_all_depgraphs.R")
